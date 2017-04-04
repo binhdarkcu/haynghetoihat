99b063e6d8d4b95f963a0d6cfb982dbe75d27efc
binhdarkcu <binhdarkcu@gmail.com>
2017-04-05 00:10:54 +0700

    category list

--- app/components/ViewAllCategory/View.js
+++ app/components/ViewAllCategory/View.js
@@ -5,17 +5,17 @@ const ViewAllCategory = () =>
     <section className="home">
         <div className="head">
             <h3 className="title">ĐANG XEM</h3>
-            <a href="#" className="viewall">Tất cả</a>
+            <a href="/listcategory" className="viewall">Tất cả</a>
         </div>
         <CategoryList categoryType = {'viewing'}/>
         <div className="head">
             <h3 className="title">THVL ĐỀ XUẤT</h3>
-            <a href="#" className="viewall">Tất cả</a>
+            <a href="/listcategory" className="viewall">Tất cả</a>
         </div>
         <CategoryList categoryType = {'recommend'}/>
         <div className="head">
             <h3 className="title">TV SHOW</h3>
-            <a href="#" className="viewall">Tất cả</a>
+            <a href="/listcategory" className="viewall">Tất cả</a>
         </div>
         <CategoryList categoryType = {'viewing'}/>
         <div className="head">
